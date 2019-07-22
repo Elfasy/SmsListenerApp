258c5301d65b273d2704e6580dc62adcf40d16cd
affily.ajat <affily.ajat@gmail.com>
2019-07-22 17:18:17 +0700

    menambahkan download receiver

--- app/src/main/java/com/example/smslistenerapp/MainActivity.java
+++ app/src/main/java/com/example/smslistenerapp/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int SMS_REQUEST_CODE = 101;
-    Button btnCheckPermission;
+    public static final String ACTION_DOWNLOAD_STATUS = "download_status";
+    private BroadcastReceiver downloadReceiver;
+    Button btnCheckPermission,btnDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnCheckPermission = findViewById(R.id.btn_permission);
         btnCheckPermission.setOnClickListener(this);
+        btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(this);
+        downloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(DownloadService.TAG,"Download selesai");
+                Toast.makeText(context,"Download selesai",Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(ACTION_DOWNLOAD_STATUS);
+
+        registerReceiver(downloadReceiver,intentFilter);
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btn_permission) {
             PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
+        }else if(view.getId() == R.id.btn_download){
+            Intent downloadServiceIntent = new Intent(MainActivity.this,DownloadService.class);
+            startService(downloadServiceIntent);
         }
     }
 
@@ -39,6 +61,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else {
                 Toast.makeText(this, "sms permission ditolak", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (downloadReceiver != null){
+            unregisterReceiver(downloadReceiver);
         }
     }
 }
